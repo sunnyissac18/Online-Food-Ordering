cd2997d9e571e1c51c26c10c04133e668398d8cb
sunnyissac18 <sunnyissac567@gmail.com>
2026-05-07 19:36:47 +0530

    Renamed tables to bypass corrupted Aiven DB

--- backend/src/main/java/com/sunny/model/Cart.java
+++ backend/src/main/java/com/sunny/model/Cart.java
@@ -9,6 +9,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 @Entity
+@Table(name = "carts")
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
--- backend/src/main/java/com/sunny/model/CartItem.java
+++ backend/src/main/java/com/sunny/model/CartItem.java
@@ -9,6 +9,7 @@ import lombok.NoArgsConstructor;
 import java.util.List;
 
 @Entity
+@Table(name = "cart_items")
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
